787a00e8a2da43124583b138388a4e3090d04df5
Maria Korneeva <korneevamg@gmail.com>
2022-01-15 20:11:56 +0100

    initial commit

---
+++ apps/proxy-for-angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'proxy-for-angular-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'proxy-for-angular';
+}
